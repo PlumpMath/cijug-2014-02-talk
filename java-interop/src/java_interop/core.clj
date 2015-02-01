c7b94038125031de26c73f37ee75d9ab4f137f13
Christopher Freeman <cwfreeman@gmail.com>
2015-02-01 12:30:52 -0600

    Let's move the contents of the StringBuffer from the constructor call into an append() call.

--- java-interop/src/java_interop/core.clj
+++ java-interop/src/java_interop/core.clj
@@ -1,7 +1,8 @@
 (ns java-interop.core)
 
 (defn make-hello []
-  (let [sb (java.lang.StringBuilder. "Hello, World!")]
+  (let [sb (java.lang.StringBuilder.)]
+    (.append sb "Hello, World!")
     sb))
 
 (defn -main []
