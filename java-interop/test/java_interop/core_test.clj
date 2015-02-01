93cdb9b6113ec43fc3584ee49bc1ff1ab124baaf
Christopher Freeman <cwfreeman@gmail.com>
2015-02-01 15:17:21 -0600

    Maybe this is ridiculous...

--- java-interop/test/java_interop/core_test.clj
+++ java-interop/test/java_interop/core_test.clj
@@ -13,10 +13,11 @@
 (deftest metric-spaces
   (testing "cartesian distances are familiar"
     (are [expected a b] (= expected (distance (->TwoDCartesian a) (->TwoDCartesian b)))
-         5 [3 4] [0 0]
-         0 [5 5] [5 5]
+         5  [3 4] [0 0]
+         0  [5 5] [5 5]
          13 [0 0] [5 12]
          13 [0 0] [12 5]
          13 [10 4] [15 -8]
-         5 [1 3 4] [1 0 0]
-         10 [1 -3 -4] [1 3 4])))
+         5  [1 3 4] [1 0 0]
+         10 [1 -3 -4] [1 3 4]
+         2  [0 0 0 0] [1 1 1 1])))
