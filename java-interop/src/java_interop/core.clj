cd1162cc87b25c3d617ba8778621bc04bb268ee9
Christopher Freeman <cwfreeman@gmail.com>
2015-02-01 14:17:48 -0600

    We can create classes in Clojure in several ways. This is 'defrecord'.
    You'll notice that 'defrecord' looks remarkably similar to 'reify'.
    
    One big difference is that 'defrecord' requires a list of fields, which we have left empty here.

--- java-interop/src/java_interop/core.clj
+++ java-interop/src/java_interop/core.clj
@@ -2,9 +2,14 @@
   (:import com.cwfreeman.StringEngine
            com.cwfreeman.StringWorker))
 
+(defrecord IdentityStringWorker []
+  com.cwfreeman.StringWorker
+  (map [this x] x)
+  (reduce [this xs] (apply str xs)))
+
 (defn make-hello2 []
   (let [engine (com.cwfreeman.StringEngine. (into-array String ["Hello" "," " " "World" "!"]))]
-    (.work engine ???)))
+    (.work engine IdentityStringWorker)))
 
 (defn make-hello []
   (doto (java.lang.StringBuilder.)
