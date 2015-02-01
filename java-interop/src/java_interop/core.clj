aaa46eb544f19c1eabc907906738d148537ee5db
Christopher Freeman <cwfreeman@gmail.com>
2015-02-01 12:29:46 -0600

    Just a little more extraction...

--- java-interop/src/java_interop/core.clj
+++ java-interop/src/java_interop/core.clj
@@ -1,7 +1,8 @@
 (ns java-interop.core)
 
 (defn make-hello []
-  (java.lang.StringBuilder. "Hello, World!"))
+  (let [sb (java.lang.StringBuilder. "Hello, World!")]
+    sb))
 
 (defn -main []
   (println (.toLowerCase (.toString (make-hello)))))
