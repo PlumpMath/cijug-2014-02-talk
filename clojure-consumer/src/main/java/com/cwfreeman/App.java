1acc010d15088cb597a3c89ff20103ad2d7d4647
Christopher Freeman <cwfreeman@gmail.com>
2015-02-01 23:32:54 -0600

    This is a little embarrassing.
    I looked and I looked, but I couldn't find a way to load a CLASS created by defrecord through the clojure.java.api.Clojure API.
    
    I BELIEVE it has something to do with when the class is generated.
    
    I KNOW that the namespace is a bit munged for the generated class.

--- clojure-consumer/src/main/java/com/cwfreeman/App.java
+++ clojure-consumer/src/main/java/com/cwfreeman/App.java
@@ -8,6 +8,30 @@ import com.cwfreeman.StringEngine;
 
 public class App 
 {
+    static Class<StringWorker> getWorkerClass() throws Exception {
+        StringWorker sw = makeWorker();
+        System.out.println("Getting class of worker: " + sw.getClass().getName());
+        /*
+        return (Class<StringWorker>)sw.getClass();
+        */
+        IFn require = Clojure.var("clojure.core", "require");
+        IFn ns_map = Clojure.var("clojure.core", "ns-map");
+        System.out.println("Namespace Map: " + ns_map.invoke(Clojure.read("java-interop.core")));
+        IFn deref = Clojure.var("clojure.core", "deref");
+        require.invoke(Clojure.read("java-interop.core"));
+        IFn isw = Clojure.var("java-interop.core", "IdentityStringWorker");
+        Object o = deref.invoke(isw);
+        /*
+        IFn clazz = Clojure.var("java_interop.core", "->IdentityStringWorker");
+        Object o = deref.invoke(clazz);
+        */
+        if( o instanceof Class ) {
+            return (Class<StringWorker>)o;
+        } else {
+            throw new RuntimeException("Got wrong thing!: " + o.toString() + o.getClass().getName());
+        }
+    }
+
     static StringWorker makeWorker() {
         IFn require = Clojure.var("clojure.core", "require");
         require.invoke(Clojure.read("java-interop.core"));
@@ -23,6 +47,12 @@ public class App
     {
         StringEngine e = new StringEngine(new String[]{"Hello", ", ", " ", "World", "!"});
         StringWorker w = makeWorker();
-        System.out.println( "Output: " + e.work(w));
+        try {
+            Class<StringWorker> wc = getWorkerClass();
+            System.out.println( "Output: " + e.work(wc));
+        } catch(Exception ex) {
+            System.err.println("Well, that didn't work.");
+            System.err.println(ex.getMessage());
+        }
     }
 }
